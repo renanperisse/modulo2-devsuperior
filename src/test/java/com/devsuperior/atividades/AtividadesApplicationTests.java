f65e03b9c938ea584eda9592fb088a5c116a95c7
Renan Perissé <renan.oliveira@xbrain.com.br>
2024-12-03 20:51:55 -0300

    Cria entidades e realiza os inserts

---
+++ src/test/java/com/devsuperior/atividades/AtividadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.atividades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
